ca6482c29a6723fdc7663dfe5d9da91150665ad8
Dmitry Alexandrovsky <dimi@uni-bremen.de>
2021-01-28 03:01:18 +0100

    WIP East Maze Transform

--- Initiation/Assets/Initiation/Scripts/Maze/InitiationMazeTransformer.cs
+++ Initiation/Assets/Initiation/Scripts/Maze/InitiationMazeTransformer.cs
@@ -2,12 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
+
 
 [RequireComponent(typeof(InitiationMazeSpawner))]
 public class InitiationMazeTransformer : MonoBehaviour
 {
     InitiationMazeSpawner maze;
-
+    List<GameObject> cellsToMove;
     public enum MoveDirection {
 		North,
 		South,
@@ -26,20 +28,52 @@ public class InitiationMazeTransformer : MonoBehaviour
 
     public void AfterCellsMoved() {
         print("cells moved");
-	}
+        if(moveDirection == MoveDirection.North) {
+            maze.MazeCells[cellIdx].Remove(cellsToMove.Last());
+            Destroy(cellsToMove.Last());
+        } else if(moveDirection == MoveDirection.South) {
+            maze.MazeCells[cellIdx].Remove(cellsToMove.First());
+            Destroy(cellsToMove.First());
+        } else if(moveDirection == MoveDirection.East) {
+            Destroy(cellsToMove.First());
+        } else if(moveDirection == MoveDirection.West) {
+            
+        }
+
+    }
 
     // Update is called once per frame
     void Update()
     {
 
         if(Input.GetKeyDown(KeyCode.Space)) {
-            List<GameObject> cellsToMove = new List<GameObject>();
+            cellsToMove = new List<GameObject>();
 
             if(moveDirection == MoveDirection.North || moveDirection == MoveDirection.South) {
                 cellsToMove.AddRange(maze.MazeCells[cellIdx]);
+                if(moveDirection == MoveDirection.North) {
+                    GameObject newCell = Instantiate(cellsToMove.Last());
+                    newCell.transform.position = cellsToMove.First().transform.position - new Vector3(0,0,maze.CellWidth);
+                    cellsToMove.Insert(0,newCell);
+                    maze.MazeCells[cellIdx].Insert(0,newCell);
+                } else {
+                    GameObject newCell = Instantiate(cellsToMove.First());
+                    newCell.transform.position = cellsToMove.Last().transform.position + new Vector3(0,0,maze.CellWidth);
+                    cellsToMove.Add(newCell);
+                    maze.MazeCells[cellIdx].Add(newCell);
+                }
             } else {
                 for(int i = 0; i < maze.Columns; i++) {
                     cellsToMove.Add(maze.MazeCells[i][cellIdx]);
+                }
+                if(moveDirection == MoveDirection.East) {
+
+                    GameObject newCell = Instantiate(cellsToMove.Last());
+                    newCell.transform.position = cellsToMove.First().transform.position + new Vector3(maze.CellWidth,0,0);
+                    cellsToMove.Add(newCell);
+                    maze.MazeCells[0][cellIdx] = newCell;
+
+                } else {
                 }
             }
 
@@ -61,11 +95,11 @@ public class InitiationMazeTransformer : MonoBehaviour
 			ht.Add(iT.MoveBy.easetype,iTween.EaseType.linear);
 
             for(int i = 0; i < cellsToMove.Count; i++) {
-                if(i == cellsToMove.Count - 1) {
-                    ht.Add(iT.MoveBy.oncomplete,"AfterCellsMoved");
-                    ht.Add(iT.MoveBy.oncompletetarget,gameObject);
-                }
-                iTween.MoveBy(cellsToMove[i],ht);
+				if(i == cellsToMove.Count - 1) {
+					ht.Add(iT.MoveBy.oncomplete,"AfterCellsMoved");
+					ht.Add(iT.MoveBy.oncompletetarget,gameObject);
+				}
+				iTween.MoveBy(cellsToMove[i],ht);
             }
             
         }
