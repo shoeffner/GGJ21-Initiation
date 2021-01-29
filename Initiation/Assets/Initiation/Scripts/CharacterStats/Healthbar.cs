9206d7712d0cac2089784695deb1b7c662abdee1
Sebastian Höffner <info@sebastian-hoeffner.de>
2021-01-29 23:16:47 +0100

    Adding Healthbars!

--- Initiation/Assets/Initiation/Scripts/CharacterStats/Healthbar.cs
+++ Initiation/Assets/Initiation/Scripts/CharacterStats/Healthbar.cs
@@ -1,6 +1,60 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(CharacterStats))]
 public class Healthbar : MonoBehaviour
 {
+    public GameObject healthbar;
+    public float heightAboveGround = 1.8f;
+    private GameObject m_healthbar;
+    private Slider slider;
+    private CharacterStats characterStats;
+    private RectTransform rectTransform;
+
+    void Start()
+    {
+        if (characterStats == null)
+        {
+            characterStats = GetComponent<CharacterStats>();
+        }
+        if (m_healthbar == null)
+        {
+            m_healthbar = Instantiate(healthbar);
+            m_healthbar.transform.SetParent(transform, false);
+            slider = m_healthbar.GetComponentInChildren<Slider>();
+            if (slider == null)
+            {
+                Debug.LogWarning($"Healthbar of {name} has no Slider component");
+            }
+            rectTransform = m_healthbar.GetComponent<RectTransform>();
+            if (rectTransform == null)
+            {
+                Debug.LogWarning($"Healthbar of {name} has no RectTransform component (no canvas?)");
+            }
+            UpdateRectTransform();
+        }
+    }
+
+    void UpdateRectTransform()
+    {
+        if (rectTransform != null)
+        {
+            rectTransform.position.Set(rectTransform.position.x, heightAboveGround, rectTransform.position.z);
+        }
+    }
+
+    void Update()
+    {
+        if (m_healthbar != null)
+        {
+            UpdateRectTransform();
+            m_healthbar.transform.LookAt(Camera.main.transform);
+        }
+        if (slider != null)
+        {
+            slider.minValue = 0;
+            slider.maxValue = characterStats.maxHealth;
+            slider.value = characterStats.health;
+        }
+    }
 }
