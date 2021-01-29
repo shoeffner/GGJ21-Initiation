4ac084a739e7bd335052f697ebe6501ef8636375
Sebastian Höffner <info@sebastian-hoeffner.de>
2021-01-29 21:59:51 +0100

    Adding basic health and damage stats

---
+++ Initiation/Assets/Initiation/Scripts/CharacterStats/DamageTriggerTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Mirror;
+
+
+[RequireComponent(typeof(Collider))]
+public class DamageTriggerTarget : NetworkBehaviour
+{
+    public int damage = 1;
+    
+    void OnTriggerEnter(Collider collider)
+    {
+        CharacterStats characterStats = collider.gameObject.GetComponent<CharacterStats>();
+        if (characterStats != null)
+        {
+            characterStats.TakeDamage(damage);
+        }
+    }
+}
---
+++ Initiation/Assets/Initiation/Scripts/CharacterStats/HealTriggerTarget.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Mirror;
+
+
+[RequireComponent(typeof(Collider))]
+public class HealTriggerTarget : NetworkBehaviour
+{
+    public int health = 5;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        CharacterStats characterStats = collider.gameObject.GetComponent<CharacterStats>();
+        if (characterStats != null)
+        {
+            characterStats.Heal(health);
+        }
+    }
+}
